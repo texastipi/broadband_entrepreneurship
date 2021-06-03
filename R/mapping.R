bfab5081f487ba6adbed51fbdbe30b68eed84ab5
jwroycechoi <jwroycechoi@gmail.com>
2021-06-03 13:27:44 -0500

    generating maps

--- R/mapping.R
+++ R/mapping.R
@@ -1,6 +1,7 @@
 library(tigris)
 library(sf)
 library(tidyverse)
+library(ggplot2)
 options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
 
@@ -16,22 +17,35 @@ ks_bb_entrepreneur_merged_v2 <- allstate_import %>%
 ks_county <- counties("Kansas")
 ks_bb_entrepreneur_merged_v3 <- sp::merge(ks_county, ks_bb_entrepreneur_merged_v2,
                                           by.x = "GEOID", by.y = "county_FIPS")
-
+## All data
+county <- counties(c("Texas", "Kansas", "Maine"))
+allstate_geo <- sp::merge(county, allstate_import,
+                          by.x = "GEOID", by.y = "county_FIPS")
 ## Getting the Metro & Micro areas
+tx <- tracts("TX", cb = T)
+me <- tracts("ME", cb = T)
 ks <- tracts("KS", cb = T)
 cb <- core_based_statistical_areas(cb = T)
 str(cb)
-# Filter for Kansas metro
+# Filter for metro, each state
 kmsa <- cb %>% 
   filter(grepl("KS", NAME)) %>% 
+  filter(LSAD == "M1")
+txmsa <- cb %>% 
+  filter(grepl("TX", NAME)) %>% 
+  filter(LSAD == "M1")
+memsa <- cb %>% 
+  filter(grepl("ME", NAME)) %>% 
   filter(LSAD == "M1")
 
 ggplot(kmsa) + geom_sf()
 
-# Leave only within KS
-w1 <- st_within(ks, kmsa)
+# Leave only within states
+w1_ks <- st_within(ks, kmsa)
+w1_tx <- st_within(tx, txmsa)
+w1_me <- st_within(me, memsa)
 
-w2 <- map_lgl(w1, function(x) {
+w2_ks <- map_lgl(w1_ks, function(x) {
   if (length(x) == 1) {
     return(TRUE)
   } else {
@@ -39,174 +53,227 @@ w2 <- map_lgl(w1, function(x) {
   }
 })
 
+w2_tx <- map_lgl(w1_tx, function(x) {
+  if (length(x) == 1) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+})
+
+w2_me <- map_lgl(w1_me, function(x) {
+  if (length(x) == 1) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+})
 # Subset 
-kmsa2 <- ks[w2,]
-kmsa2 <- kmsa2 %>% 
-  st_union()
-# Plot
-ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct25_3_dec_2019_fcc), lwd = 0) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "FCC Broadband\n at 25/3 Mbps (%)",
-                       label = scales::percent) +
-  geom_sf(data = kmsa2, fill = alpha("grey50", 0.5), color = "white") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
+kmsa2 <- ks[w2_ks,] %>% st_union()
+txmsa2 <- tx[w2_tx,] %>% st_union()
+memsa2 <- me[w2_me,] %>% st_union()
 
-#### Kansas FCC & MS ####
+#### Maps ####
+state_ls <- c("KS", "TX", "ME")
+msa_ls <- list(kmsa2, txmsa2, memsa2)
+fcc_plot_ls <- list()
+ms_plot_ls <- list()
+acs_plot_ls <- list()
 ## FCC 25/3 Mbps
-ks_fcc_map <- ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct25_3_dec_2019_fcc), lwd = 0.1) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "FCC Broadband\n at 25/3 Mbps (%)",
-                       label = scales::percent) +
-  labs(caption = "Source: FCC Form477 Data as of December 2019") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
-
+# Loop to create a list of maps
+for (i in 1:length(state_ls)) {
+  fcc_plot_ls[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = pct25_3_dec_2019_fcc), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "FCC Broadband\n at 25/3 Mbps (%)",
+                         label = scales::percent) +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory2", 1), lwd = 0.7) +
+    labs(caption = "Source: FCC Form477 Data as of December 2019") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold", hjust = 0.5),
+          plot.caption = element_text(face = "italic"))
+}
 ## MS Broadband
-ks_ms_map <- ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct_broadband_MS), lwd = 0.1) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "Microsoft\n Broadband (%)",
-                       label = scales::percent) +
-  labs(caption = "Source: Microsoft as of November 2019") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
-
+for (i in 1:length(state_ls)) {
+  ms_plot_ls[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = pct_broadband_MS), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "Microsoft\n Broadband (%)",
+                         label = scales::percent) +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory4", 1), lwd = 0.7) +
+    labs(caption = "Source: Microsoft as of November 2019") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold", hjust = 0.5),
+          plot.caption = element_text(face = "italic"))
+}
+# Export maps
 ggsave("ks-fcc-map.png",
-       plot = ks_fcc_map, width = 10, height = 6, units = "in", dpi = 600)
-
+       plot = fcc_plot_ls[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-fcc-map.png",
+       plot = fcc_plot_ls[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-fcc-map.png",
+       plot = fcc_plot_ls[[3]], width = 10, height = 6, units = "in", dpi = 600)
 ggsave("ks-ms-map.png",
-       plot = ks_ms_map, width = 10, height = 6, units = "in", dpi = 600)
-
-## FCC 25/3 Histogram
-ks_fcc_hist <- ggplot(ks_bb_entrepreneur_merged_v2, aes(x = pct25_3_dec_2019_fcc)) + 
-  geom_histogram() + theme_minimal() + 
-  theme(axis.text = element_text(size = 11),
-        axis.title = element_text(size = 12, face = "bold")) + 
-  xlab("FCC Broadband at 25/3 Mbps (%)") + ylab("Frequency") +
-  scale_x_continuous(labels = scales::percent, breaks = scales::breaks_pretty()) +
-  labs(caption = "Source: FCC Form477 Data as of December 2019")
-
-## MS Broadband
-ks_ms_hist <- ggplot(ks_bb_entrepreneur_merged_v2, aes(x = pct_broadband_MS)) + 
-  geom_histogram() + theme_minimal() + 
-  theme(axis.text = element_text(size = 11),
-        axis.title = element_text(size = 12, face = "bold")) + 
-  xlab("Microsoft Broadband (%)") + ylab("Frequency") +
-  scale_x_continuous(labels = scales::percent, breaks = scales::breaks_pretty(),
-                     limits = c(0,1)) +
-  labs(caption = "Source: Microsoft as of November 2019")
-
-ggsave("ks-fcc-hist.png",
-       plot = ks_fcc_hist, width = 10, height = 6, units = "in", dpi = 600)
-
-ggsave("ks-ms-hist.png",
-       plot = ks_ms_hist, width = 10, height = 6, units = "in", dpi = 600)
+       plot = ms_plot_ls[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-ms-map.png",
+       plot = ms_plot_ls[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-ms-map.png",
+       plot = ms_plot_ls[[3]], width = 10, height = 6, units = "in", dpi = 600)
 
 #### ACS Any broadband ####
 library(censusapi)
-acs_ks_anybb <- getCensus(name = "acs/acs5/subject", vintage = 2019,
+# Add key to .Renviron
+Sys.setenv(CENSUS_KEY="9001b546c2d77876a089119664dc25a4235eea37")
+# Reload .Renviron
+readRenviron("~/.Renviron")
+# Check to see that the expected key is output in your R console
+Sys.getenv("CENSUS_KEY")
+
+acs_anybb <- getCensus(name = "acs/acs5/subject", vintage = 2019,
                           vars = "S2801_C02_014E",
                           region = "county:*",
-                          regionin = "state:20")
-str(acs_ks_anybb)
-glimpse(ks_bb_entrepreneur_merged_v3)
+                          regionin = "state:20,23,48")
 
-acs_ks_anybb <- acs_ks_anybb %>% 
+acs_anybb <- acs_anybb %>% 
   mutate(geoid = paste0(state, county))
-str(acs_ks_anybb)
+str(acs_anybb)
 
-ks_bb_entrepreneur_merged_v3 <- left_join(ks_bb_entrepreneur_merged_v3,
-                                          select(acs_ks_anybb, geoid, S2801_C02_014E),
-                                          by = c("GEOID" = "geoid")) %>% 
+allstate_geo <- left_join(allstate_geo,
+                          select(acs_anybb, geoid, S2801_C02_014E),
+                          by = c("GEOID" = "geoid")) %>% 
   mutate(pct_anybroadband_acs_2019 = S2801_C02_014E/100)
 
-glimpse(ks_bb_entrepreneur_merged_v3)
+glimpse(allstate_geo)
 
-## Map Kansas ACS Any broadband
-
-ks_acs_map <- ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct_anybroadband_acs_2019), lwd = 0) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "ACS Household with\nAny Broadband Subscription (%)",
-                       label = scales::percent) +
-  geom_sf(data = kmsa2, fill = alpha("honeydew3", 0.3), color = alpha("royalblue3", 1), lwd = 0.6) +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
-
+## Map ACS Any broadband
+for (i in 1:length(state_ls)) {
+  acs_plot_ls[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = pct_anybroadband_acs_2019), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "Households with\nAny Broadband\nSubscription (%)",
+                         label = scales::percent) +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory2", 1), lwd = 0.7) +
+    labs(caption = "Source: American Community Survey 5-year Estimates (2019)") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold", hjust = 0.5),
+          plot.caption = element_text(face = "italic"))
+}
+# Export maps
 ggsave("ks-acs-map.png",
-       plot = ks_acs_map, width = 10, height = 6, units = "in", dpi = 600)
+       plot = acs_plot_ls[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-acs-map.png",
+       plot = acs_plot_ls[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-acs-map.png",
+       plot = acs_plot_ls[[3]], width = 10, height = 6, units = "in", dpi = 600)
 
-#### Sole-Proprietorship map ####
-
+#### Entrepreneurship map ####
+nf_prop_maps <- list()
+nf_prop_chg_maps <- list()
+vd_maps <- list()
 ## % of nonfarm proprietorship
-ks_nonfarm_map <- ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct_nonfarm_bea_2018), lwd = 0.1) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "Nonfarm Proprietorship (%)",
-                       label = scales::percent) +
-  labs(caption = "Source: Bureau of Economic Analysis (2018)") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
-
-ggsave("ks-nonfarm-map.png",
-       plot = ks_nonfarm_map, width = 10, height = 6, units = "in", dpi = 600)
+for (i in 1:length(state_ls)) {
+  nf_prop_maps[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = pct_nonfarm_bea_2018), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "Nonfarm\nProprietorship (%)",
+                         label = scales::percent) +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory2", 1), lwd = 0.7) +
+    labs(caption = "Source: Bureau of Economic Analysis (2018)") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold", hjust = 0.5),
+          plot.caption = element_text(face = "italic"))
+}
 
 ## Change in nonfarm proprietorship
-ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = pct_chg_bea_2012_2018), lwd = 0.1) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "Change in Nonfarm\nProprietorship 2012-2018 (%)",
-                       label = scales::percent) +
-  labs(caption = "Source: Bureau of Economic Analysis (2012, 2018)") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
+for (i in 1:length(state_ls)) {
+  nf_prop_chg_maps[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = pct_chg_bea_2012_2018), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "Change in Nonfarm\nProprietorship\n2012-2018 (%)",
+                         label = scales::percent) +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory2", 1), lwd = 0.7) +
+    labs(caption = "Source: Bureau of Economic Analysis (2012, 2018)") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold", hjust = 0.5),
+          plot.caption = element_text(face = "italic"))
+}
 
 ## Average venture density 2019-2020
-
-ks_bb_entrepreneur_merged_v3 <- ks_bb_entrepreneur_merged_v3 %>% 
+allstate_geo <- allstate_geo %>% 
   mutate(vd_mean = (vd_mean_19 + vd_mean_20)/2)
 
-ks_vd_map <- ggplot() + 
-  geom_sf(data = ks_bb_entrepreneur_merged_v3, aes(fill = vd_mean), lwd = 0.1) +
-  scale_fill_gradientn(colours = brewer.pal(5, "YlOrRd"),
-                       name = "Average Venture Density\n(per 100 people)") +
-  labs(caption = "Source: GoDaddy (2019-2020)") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank())
+for (i in 1:length(state_ls)) {
+  vd_maps[[i]] <- allstate_geo %>% filter(ST == state_ls[i]) %>% 
+    ggplot() + 
+    geom_sf(aes(fill = vd_mean), lwd = 0.1) +
+    scale_fill_gradientn(colours = RColorBrewer::brewer.pal(5, "YlOrRd"),
+                         name = "Average\nVenture\nDensity\n(per 100 people)") +
+    geom_sf(data = msa_ls[[i]], fill = alpha("honeydew3", 0), color = alpha("ivory4", 1), lwd = 0.7) +
+    labs(caption = "Source: GoDaddy (2019-2020)") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(),
+          legend.title = element_text(face = "bold"),
+          plot.caption = element_text(face = "italic"))
+}
 
+# Export maps
+# Nonfarm proprietorship
+ggsave("ks-nonfarm-map.png",
+       plot = nf_prop_maps[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-nonfarm-map.png",
+       plot = nf_prop_maps[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-nonfarm-map.png",
+       plot = nf_prop_maps[[3]], width = 10, height = 6, units = "in", dpi = 600)
+
+# Nonfarm proprietorship change
+ggsave("ks-nonfarm-chg-map.png",
+       plot = nf_prop_chg_maps[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-nonfarm-chg-map.png",
+       plot = nf_prop_chg_maps[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-nonfarm-chg-map.png",
+       plot = nf_prop_chg_maps[[3]], width = 10, height = 6, units = "in", dpi = 600)
+
+# Venture density
 ggsave("ks-vd-map.png",
-       plot = ks_vd_map, width = 10, height = 6, units = "in", dpi = 600)
+       plot = vd_maps[[1]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("tx-vd-map.png",
+       plot = vd_maps[[2]], width = 10, height = 6, units = "in", dpi = 600)
+ggsave("me-vd-map.png",
+       plot = vd_maps[[3]], width = 10, height = 6, units = "in", dpi = 600)
 
 
 
